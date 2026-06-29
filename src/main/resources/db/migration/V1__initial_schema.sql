c0dce67905ac86f66538258854d83dd8b7e59b77
vedant <vedantguptadsa@gmail.com>
2026-06-29 11:12:44 +0530

    url  Schema

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -5,3 +5,17 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+------------------------------------------------------------
+
+CREATE TABLE urls (
+    id BIGSERIAL PRIMARY KEY,
+    original_url TEXT NOT NULL,
+    short_code VARCHAR(20) NOT NULL UNIQUE,
+    user_id BIGINT NOT NULL,
+
+    CONSTRAINT fk_urls_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
