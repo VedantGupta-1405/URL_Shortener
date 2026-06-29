0ea6a8036eebfaf7fce94abc381d1e5ac31ff0bb
vedant <vedantguptadsa@gmail.com>
2026-06-29 11:27:02 +0530

    refresh tokens Schema

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -13,8 +13,26 @@ CREATE TABLE urls (
     original_url TEXT NOT NULL,
     short_code VARCHAR(20) NOT NULL UNIQUE,
     user_id BIGINT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    expires_at TIMESTAMP,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
 
     CONSTRAINT fk_urls_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
+
+-------------------------------------------------------------
+
+CREATE TABLE refresh_tokens (
+    id BIGSERIAL PRIMARY KEY,
+    token TEXT NOT NULL UNIQUE,
+    user_id BIGINT NOT NULL,
+    expires_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_refresh_tokens_user
         FOREIGN KEY (user_id)
         REFERENCES users(id)
         ON DELETE CASCADE
