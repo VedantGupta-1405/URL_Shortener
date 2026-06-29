c5c7d042e04e58a281d59346e3cdd8a0387a7f40
vedant <vedantguptadsa@gmail.com>
2026-06-29 13:10:07 +0530

    entity class initial

---
+++ src/main/java/com/vedant/url_shortener/entity/Url.java
@@ -0,0 +1,10 @@
+package com.vedant.url_shortener.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "urls")
+public class Url {
+
+}
---
+++ src/main/java/com/vedant/url_shortener/entity/User.java
@@ -0,0 +1,26 @@
+package com.vedant.url_shortener.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+}
